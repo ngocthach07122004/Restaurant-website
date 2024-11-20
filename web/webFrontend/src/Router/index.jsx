9e087594d1fea7b58d46fcc15e962398ba89b4d4
Luciferstrike123 <130897548+Luciferstrike123@users.noreply.github.com>
2024-11-20 15:15:16 +0700

    Adding basic admin components

--- web/webFrontend/src/Router/index.jsx
+++ web/webFrontend/src/Router/index.jsx
@@ -4,6 +4,16 @@ import Signin from "../pages/Signin";
 import Signup from "../pages/Signup";
 import Menu from "../pages/Menu";
 import DefaultLayout from "../layout/DefaultLayout";
+import AdminLayout from "../layout/Admin/AdminLayout";
+import AdminDashboard from "../pages/Admin/Dashboard";
+
+// const checkAdminAuth = async () => {
+//     const user = await fetch('/api/check-auth'); 
+//     if (!user || user.role !== 'admin') {
+//       throw new Error('Unauthorized');
+//     }
+//     return user;
+//   };
 
 const router = createBrowserRouter([
     {
@@ -15,20 +25,15 @@ const router = createBrowserRouter([
             {path: "signup/", element: <Signup />},
             {path: "menu/", element: <Menu />}
         ],
-        // element: <Home />
     },
-    // {
-    //     path: "signin/",
-    //     element: <Signin />
-    // },
-    // {
-    //     path: "signup/",
-    //     element: <Signup />
-    // },
-    // {
-    //     path: "menu/",
-    //     element: <Menu />
-    // },
+    {
+        path: "/admin/",
+        element: <AdminLayout />,
+        // loader: checkAdminAuth, // Secure admin routes
+        children: [
+            {index: true, element: <AdminDashboard />}
+        ]
+    }
 ]);
 
 export default router;
---
+++ web/webFrontend/src/pages/Admin/Dashboard/index.jsx
@@ -0,0 +1,11 @@
+const AdminDashboard = () => {
+    return (
+      <div>
+        <h1>Admin Dashboard</h1>
+        <p>Welcome to the admin panel!</p>
+      </div>
+    );
+  };
+  
+  export default AdminDashboard;
+  
