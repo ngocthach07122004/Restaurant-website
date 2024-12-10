4528be5e375fe7a6f4896d7db5a358b150d2a8da
quanghia24 <nghiaqcqs@gmail.com>
2024-12-10 14:21:09 +0700

    local commit

--- sql/procedure1_2_1.sql
+++ sql/procedure1_2_1.sql
@@ -1,81 +1,71 @@
 create procedure handler_Insert_Table_ThongTin
 (
-     in cccd_Arg varchar(255),
-in anhThongTin_Arg varchar(255),
-in email_Arg varchar(255),
-in gioiTinh_Arg char(1),
-in ho_Arg varchar(255),
-in maTaiKhoan_Arg varchar(255),
-in matKhau_Arg varchar(255),
-in ngaySinh_Arg date,
-in ten_Arg varchar(255),
-in tenDangNhap_Arg varchar(255),
-in cccdQuanTriVien_Arg varchar(255)
+   in cccd_Arg             varchar(255),
+   in anhThongTin_Arg      varchar(255),
+   in email_Arg            varchar(255),
+   in gioiTinh_Arg         char(1),
+   in ho_Arg               varchar(255),
+   in maTaiKhoan_Arg       varchar(255),
+   in matKhau_Arg          varchar(255),
+   in ngaySinh_Arg         date,
+   in ten_Arg              varchar(255),
+   in tenDangNhap_Arg      varchar(255),
+   in cccdQuanTriVien_Arg  varchar(255)
 )
 begin
-    if exists ( select 1 from ThongTin where cccd_Arg = ThongTin.cccd  ) 
-    then 
-       SIGNAL SQLSTATE '45000'
-       SET MESSAGE_TEXT = 'Căn cước đã tồn tại trong hệ thống, vui lòng kiểm tra lại số căn cước'; 
-    end if;
-    if cccd_Arg is null or cccd_Arg = ''
-    then 
-       SIGNAL SQLSTATE '45000'
-       SET MESSAGE_TEXT = 'Trường này không được để trống, đây là khóa của dữ liệu'; 
-    end if;
-
-    if CHAR_LENGTH(tenDangNhap_Arg) <6 and tenDangNhap_Arg not like ' %'
-    then 
-       SIGNAL SQLSTATE '45000'
-       SET MESSAGE_TEXT = 'Tên đăng nhập phải lớn hơn 5 ký tự'; 
-    end if;
-
-    if CHAR_LENGTH(matKhau_Arg) <8 
-    then 
-       SIGNAL SQLSTATE '45000'
-       SET MESSAGE_TEXT = 'Mật khẩu phải lớn hơn hoặc bằng 8 ký tự'; 
-    end if;
-
-    
-    if !(ngaySinh_Arg REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$')
-    then 
-       SIGNAL SQLSTATE '45000'
-       SET MESSAGE_TEXT = 'Ngày sinh phải hợp lệ'; 
-    end if;
-
-    if (maTaiKhoan_Arg = 'user')
-    then (
-      if (ngaySinh_Arg > CURDATE())
-      then
-       SIGNAL SQLSTATE '45000'
-       SET MESSAGE_TEXT = 'Ngày sinh phải lớn hơn ngày hiện tại';    
-    )
-    else
-    then
-       if ()
-    end if; 
-
-    if !REGEXP_LIKE(email_Arg, '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')
-    then 
-       SIGNAL SQLSTATE '45000'
-       SET MESSAGE_TEXT = 'Email phải hợp lệ và đúng cú pháp định dạng email'; 
-    end if;
-    
-    
-    
-    insert into ThongTin values( 
+   if exists ( select 1 from ThongTin where cccd_Arg = ThongTin.cccd  ) 
+   then 
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Căn cước đã tồn tại trong hệ thống, vui lòng kiểm tra lại số căn cước'; 
+   end if;
+   if cccd_Arg is null or cccd_Arg = ''
+   then 
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Trường này không được để trống, đây là khóa của dữ liệu'; 
+   end if;
+   if CHAR_LENGTH(tenDangNhap_Arg) <6 and tenDangNhap_Arg not like ' %'
+   then 
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Tên đăng nhập phải lớn hơn 5 ký tự'; 
+   end if;
+   if CHAR_LENGTH(matKhau_Arg) <8 
+   then 
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Mật khẩu phải lớn hơn hoặc bằng 8 ký tự'; 
+   end if;
+   if !(ngaySinh_Arg REGEXP '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$')
+   then 
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Ngày sinh phải hợp lệ'; 
+   end if;
+   if (maTaiKhoan_Arg = 'user')
+   then (
+     if (ngaySinh_Arg > CURDATE())
+     then
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Ngày sinh phải lớn hơn ngày hiện tại';    
+   )
+   else
+   then
+      if ()
+   end if; 
+   if !REGEXP_LIKE(email_Arg, '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')
+   then 
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Email phải hợp lệ và đúng cú pháp định dạng email'; 
+   end if;
+   insert into ThongTin values( 
       cccd_Arg,
-anhThongTin_Arg,
-email_Arg,
-gioiTinh_Arg,
-ho_Arg,
-maTaiKhoan_Arg,
-matKhau_Arg,
-ngaySinh_Arg,
-ten_Arg,
-tenDangNhap_Arg,
-cccdQuanTriVien_Arg); 
-    
+      anhThongTin_Arg,
+      email_Arg,
+      gioiTinh_Arg,
+      ho_Arg,
+      maTaiKhoan_Arg,
+      matKhau_Arg,
+      ngaySinh_Arg,
+      ten_Arg,
+      tenDangNhap_Arg,
+      cccdQuanTriVien_Arg); 
 end; 
 
 
